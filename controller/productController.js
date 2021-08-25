8fb35fa3272f8efed158ebbb834747bf8c8e6d87
sumit-kr-das <sumitkrdas777@gmail.com>
2021-08-25 17:03:06 +0530

    single product page api getProductByID() finished

--- controller/productController.js
+++ controller/productController.js
@@ -2,7 +2,16 @@ import Product from "../model/productSchema";
 
 export const getProducts = async (req,res) =>{
     try{
-        const product = await Product.find({})
+        const products = await Product.find({})
+        res.json(products);
+    }catch(err){
+        console.log("Error is ~ ",err.message);
+    }
+}
+
+export const getProductByID = async (req,res) =>{
+    try{
+        const product = await Product.findOne({'id':req.params.id})
         res.json(product);
     }catch(err){
         console.log("Error is ~ ",err.message);
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import {userSignup,userLogin} from '../controller/userController';
-import {getProducts} from '../controller/productController';
+import {getProducts,getProductByID} from '../controller/productController';
 
 router.post("/signup",userSignup);
 router.post("/login",userLogin);
 
 router.get("/products",getProducts);
+router.get("/product/:id",getProductByID);
 
 export default router;
